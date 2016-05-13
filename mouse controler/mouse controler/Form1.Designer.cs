928806d434e93ad8807b13475e6b32bb94760a7d
ron <cohenron7894@gmail.com>
2016-05-14 00:31:54 +0300

    Created and finished

---
+++ mouse controler/mouse controler/Form1.Designer.cs
@@ -0,0 +1,448 @@
+﻿namespace mouse_controler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SP_X = new System.Windows.Forms.TextBox();
+            this.SP_Y = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.GoTo_1_x = new System.Windows.Forms.TextBox();
+            this.GoTo_1_y = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.GoTo_2_x = new System.Windows.Forms.TextBox();
+            this.GoTo_2_y = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.GoTo_3_x = new System.Windows.Forms.TextBox();
+            this.GoTo_3_y = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.Text_2 = new System.Windows.Forms.TextBox();
+            this.GoTo_4_y = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.GoTo_4_x = new System.Windows.Forms.TextBox();
+            this.label17 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label18 = new System.Windows.Forms.Label();
+            this.Repeat = new System.Windows.Forms.TextBox();
+            this.label19 = new System.Windows.Forms.Label();
+            this.Text_1 = new System.Windows.Forms.TextBox();
+            this.GetC = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(197, 317);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(135, 58);
+            this.start.TabIndex = 0;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Starting Point";
+            // 
+            // SP_X
+            // 
+            this.SP_X.Location = new System.Drawing.Point(40, 41);
+            this.SP_X.Name = "SP_X";
+            this.SP_X.Size = new System.Drawing.Size(67, 20);
+            this.SP_X.TabIndex = 2;
+            // 
+            // SP_Y
+            // 
+            this.SP_Y.Location = new System.Drawing.Point(172, 41);
+            this.SP_Y.Name = "SP_Y";
+            this.SP_Y.Size = new System.Drawing.Size(71, 20);
+            this.SP_Y.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(149, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Y:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "X:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Go to 1:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(149, 101);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Y:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 101);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "X:";
+            // 
+            // GoTo_1_x
+            // 
+            this.GoTo_1_x.Location = new System.Drawing.Point(40, 98);
+            this.GoTo_1_x.Name = "GoTo_1_x";
+            this.GoTo_1_x.Size = new System.Drawing.Size(67, 20);
+            this.GoTo_1_x.TabIndex = 2;
+            // 
+            // GoTo_1_y
+            // 
+            this.GoTo_1_y.Location = new System.Drawing.Point(172, 98);
+            this.GoTo_1_y.Name = "GoTo_1_y";
+            this.GoTo_1_y.Size = new System.Drawing.Size(71, 20);
+            this.GoTo_1_y.TabIndex = 2;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(149, 154);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 13);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Y:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 154);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(17, 13);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "X:";
+            // 
+            // GoTo_2_x
+            // 
+            this.GoTo_2_x.Location = new System.Drawing.Point(40, 151);
+            this.GoTo_2_x.Name = "GoTo_2_x";
+            this.GoTo_2_x.Size = new System.Drawing.Size(67, 20);
+            this.GoTo_2_x.TabIndex = 2;
+            // 
+            // GoTo_2_y
+            // 
+            this.GoTo_2_y.Location = new System.Drawing.Point(172, 151);
+            this.GoTo_2_y.Name = "GoTo_2_y";
+            this.GoTo_2_y.Size = new System.Drawing.Size(71, 20);
+            this.GoTo_2_y.TabIndex = 2;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 135);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(45, 13);
+            this.label9.TabIndex = 3;
+            this.label9.Text = "Go to 2:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(149, 206);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(17, 13);
+            this.label10.TabIndex = 1;
+            this.label10.Text = "Y:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(12, 206);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(17, 13);
+            this.label11.TabIndex = 1;
+            this.label11.Text = "X:";
+            // 
+            // GoTo_3_x
+            // 
+            this.GoTo_3_x.Location = new System.Drawing.Point(40, 203);
+            this.GoTo_3_x.Name = "GoTo_3_x";
+            this.GoTo_3_x.Size = new System.Drawing.Size(67, 20);
+            this.GoTo_3_x.TabIndex = 2;
+            // 
+            // GoTo_3_y
+            // 
+            this.GoTo_3_y.Location = new System.Drawing.Point(172, 203);
+            this.GoTo_3_y.Name = "GoTo_3_y";
+            this.GoTo_3_y.Size = new System.Drawing.Size(71, 20);
+            this.GoTo_3_y.TabIndex = 2;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(12, 187);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(45, 13);
+            this.label12.TabIndex = 3;
+            this.label12.Text = "Go to 3:";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(149, 256);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(17, 13);
+            this.label13.TabIndex = 1;
+            this.label13.Text = "Y:";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(330, 101);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(24, 13);
+            this.label14.TabIndex = 1;
+            this.label14.Text = "text";
+            // 
+            // Text_2
+            // 
+            this.Text_2.Location = new System.Drawing.Point(438, 98);
+            this.Text_2.Name = "Text_2";
+            this.Text_2.Size = new System.Drawing.Size(67, 20);
+            this.Text_2.TabIndex = 2;
+            // 
+            // GoTo_4_y
+            // 
+            this.GoTo_4_y.Location = new System.Drawing.Point(172, 253);
+            this.GoTo_4_y.Name = "GoTo_4_y";
+            this.GoTo_4_y.Size = new System.Drawing.Size(71, 20);
+            this.GoTo_4_y.TabIndex = 2;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(330, 82);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(66, 13);
+            this.label15.TabIndex = 3;
+            this.label15.Text = "Starting text:";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(12, 262);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(17, 13);
+            this.label16.TabIndex = 1;
+            this.label16.Text = "X:";
+            // 
+            // GoTo_4_x
+            // 
+            this.GoTo_4_x.Location = new System.Drawing.Point(40, 259);
+            this.GoTo_4_x.Name = "GoTo_4_x";
+            this.GoTo_4_x.Size = new System.Drawing.Size(67, 20);
+            this.GoTo_4_x.TabIndex = 2;
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(12, 243);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(45, 13);
+            this.label17.TabIndex = 3;
+            this.label17.Text = "Go to 4:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.ForeColor = System.Drawing.Color.Silver;
+            this.panel1.Location = new System.Drawing.Point(271, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(16, 266);
+            this.panel1.TabIndex = 4;
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(330, 44);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(30, 13);
+            this.label18.TabIndex = 1;
+            this.label18.Text = "num:";
+            // 
+            // Repeat
+            // 
+            this.Repeat.Location = new System.Drawing.Point(358, 41);
+            this.Repeat.Name = "Repeat";
+            this.Repeat.Size = new System.Drawing.Size(67, 20);
+            this.Repeat.TabIndex = 2;
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(330, 25);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(42, 13);
+            this.label19.TabIndex = 3;
+            this.label19.Text = "Repeat";
+            // 
+            // Text_1
+            // 
+            this.Text_1.Location = new System.Drawing.Point(358, 98);
+            this.Text_1.Name = "Text_1";
+            this.Text_1.Size = new System.Drawing.Size(67, 20);
+            this.Text_1.TabIndex = 2;
+            this.Text_1.Text = "Episode";
+            // 
+            // GetC
+            // 
+            this.GetC.Location = new System.Drawing.Point(407, 341);
+            this.GetC.Name = "GetC";
+            this.GetC.Size = new System.Drawing.Size(98, 36);
+            this.GetC.TabIndex = 0;
+            this.GetC.Text = "Get coordinates";
+            this.GetC.UseVisualStyleBackColor = true;
+            this.GetC.Click += new System.EventHandler(this.GetC_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 389);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.GoTo_4_y);
+            this.Controls.Add(this.GoTo_3_y);
+            this.Controls.Add(this.GoTo_2_y);
+            this.Controls.Add(this.GoTo_1_y);
+            this.Controls.Add(this.GoTo_4_x);
+            this.Controls.Add(this.SP_Y);
+            this.Controls.Add(this.Repeat);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.Text_1);
+            this.Controls.Add(this.Text_2);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.GoTo_3_x);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.GoTo_2_x);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.GoTo_1_x);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.SP_X);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.GetC);
+            this.Controls.Add(this.start);
+            this.Name = "Form1";
+            this.Text = "Mouse Controler";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox GoTo_1_y;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox GoTo_2_x;
+        private System.Windows.Forms.TextBox GoTo_2_y;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox GoTo_3_x;
+        private System.Windows.Forms.TextBox GoTo_3_y;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox Text_2;
+        private System.Windows.Forms.TextBox GoTo_4_y;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.TextBox GoTo_4_x;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.TextBox Repeat;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.TextBox Text_1;
+        private System.Windows.Forms.TextBox GoTo_1_x;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox SP_Y;
+        private System.Windows.Forms.TextBox SP_X;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button GetC;
+    }
+}
+
---
+++ mouse controler/mouse controler/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mouse_controler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region var
+
+        public int startingPoint_x { get; set; }
+        public int GOTO_1_x { get; set; }
+        public int GOTO_2_x { get; set; }
+        public int GOTO_3_x { get; set; }
+        public int GOTO_4_x { get; set; }
+        public int repeat { get; set; }
+        public string text_1 { get; set; }
+        public int text_2 { get; set; }
+
+        public int startingPoint_y { get; set; }
+        public int GOTO_1_y { get; set; }
+        public int GOTO_2_y { get; set; }
+        public int GOTO_3_y { get; set; }
+        public int GOTO_4_y { get; set; }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+
+        #endregion
+
+        #region mouse
+
+        [DllImport("user32.dll")]
+        static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+        private const int MOUSEEVENTF_MOVE = 0x0001;
+        private const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+        private const int MOUSEEVENTF_LEFTUP = 0x0004;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x0008;
+        private const int MOUSEEVENTF_RIGHTUP = 0x0010;
+        private const int MOUSEEVENTF_MIDDLEDOWN = 0x0020;
+        private const int MOUSEEVENTF_MIDDLEUP = 0x0040;
+        private const int MOUSEEVENTF_ABSOLUTE = 0x8000;
+
+        public static void Move(int xDelta, int yDelta)
+        {
+            mouse_event(MOUSEEVENTF_MOVE, xDelta, yDelta, 0, 0);
+        }
+        public static void MoveTo(int x, int y)
+        {
+            mouse_event(MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE, x, y, 0, 0);
+        }
+        public static void LeftClick()
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN, System.Windows.Forms.Control.MousePosition.X, System.Windows.Forms.Control.MousePosition.Y, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, System.Windows.Forms.Control.MousePosition.X, System.Windows.Forms.Control.MousePosition.Y, 0, 0);
+        }
+
+        public static void LeftDown()
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN, System.Windows.Forms.Control.MousePosition.X, System.Windows.Forms.Control.MousePosition.Y, 0, 0);
+        }
+
+        public static void LeftUp()
+        {
+            mouse_event(MOUSEEVENTF_LEFTUP, System.Windows.Forms.Control.MousePosition.X, System.Windows.Forms.Control.MousePosition.Y, 0, 0);
+        }
+
+        public static void RightClick()
+        {
+            mouse_event(MOUSEEVENTF_RIGHTDOWN, System.Windows.Forms.Control.MousePosition.X, System.Windows.Forms.Control.MousePosition.Y, 0, 0);
+            mouse_event(MOUSEEVENTF_RIGHTUP, System.Windows.Forms.Control.MousePosition.X, System.Windows.Forms.Control.MousePosition.Y, 0, 0);
+        }
+
+        public static void RightDown()
+        {
+            mouse_event(MOUSEEVENTF_RIGHTDOWN, System.Windows.Forms.Control.MousePosition.X, System.Windows.Forms.Control.MousePosition.Y, 0, 0);
+        }
+
+        public static void RightUp()
+        {
+            mouse_event(MOUSEEVENTF_RIGHTUP, System.Windows.Forms.Control.MousePosition.X, System.Windows.Forms.Control.MousePosition.Y, 0, 0);
+        }
+
+        #endregion
+
+        private async void start_Click(object sender, EventArgs e)
+        {
+            #region set var
+
+            startingPoint_x = int.Parse(SP_X.Text);
+            GOTO_1_x = int.Parse(GoTo_1_x.Text);
+            GOTO_2_x = int.Parse(GoTo_2_x.Text);
+            GOTO_3_x = int.Parse(GoTo_3_x.Text);
+            GOTO_4_x = int.Parse(GoTo_4_x.Text);
+
+            startingPoint_y = int.Parse(SP_Y.Text);
+            GOTO_1_y = int.Parse(GoTo_1_y.Text);
+            GOTO_2_y = int.Parse(GoTo_2_y.Text);
+            GOTO_3_y = int.Parse(GoTo_3_y.Text);
+            GOTO_4_y = int.Parse(GoTo_4_y.Text);
+
+            repeat = int.Parse(Repeat.Text);
+            text_1 = Text_1.Text;
+            text_2 = int.Parse(Text_2.Text);
+
+            int i = text_2;
+
+            #endregion;
+            await Task.Delay(2000);
+
+            for (int f = 0; f <= repeat; f++)
+            {
+                Move(startingPoint_x, startingPoint_y);
+                LeftClick();
+                await Task.Delay(2000);
+
+                Move(GOTO_1_x, GOTO_1_y);
+                LeftClick();
+                await Task.Delay(2000);
+
+                Move(GOTO_2_x, GOTO_2_y);
+                LeftClick();
+                await Task.Delay(2000);
+
+                Move(GOTO_3_x, GOTO_3_y);
+                LeftClick();
+                await Task.Delay(2000);
+
+                SendKeys.SendWait(string.Format("{0} {1}", text_1, i));
+                await Task.Delay(1000);
+
+                Move(GOTO_4_x, GOTO_4_y);
+                LeftClick();
+                await Task.Delay(5000);
+            }                      
+        }
+
+        private async void GetC_Click(object sender, EventArgs e)
+        {
+            AllocConsole();
+            while (true)
+            {
+                Console.WriteLine(Cursor.Position.ToString());
+                await Task.Delay(200);
+                Console.Clear();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
+
+   
